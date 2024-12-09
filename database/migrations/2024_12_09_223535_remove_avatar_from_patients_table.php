e6bc3cc675a83914da94356e417e0e7438e9a4e1
Michelle <ibarracortezmichellenahomy@gmail.com>
2024-12-09 17:11:54 -0600

    Agregados cambios para recuperar el avatar y ajustar migraciones

--- database/migrations/2024_12_09_223535_remove_avatar_from_patients_table.php
+++ database/migrations/2024_12_09_223535_remove_avatar_from_patients_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('patients', function (Blueprint $table) {
-            $table->string('avatar'); // Agregar columna avatar
+            //
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('patients', function (Blueprint $table) {
-            $table->dropColumn('avatar'); // Eliminar columna avatar
+            //
         });
     }
 };
