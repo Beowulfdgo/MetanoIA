b7127cd25b2054bfe7c1bce4354777b943892047
Michelle <ibarracortezmichellenahomy@gmail.com>
2024-12-09 16:00:22 -0600

    Corregir rutas duplicadas en el archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -6,28 +6,25 @@ use App\Http\Controllers\PatientsController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PDFController;
-
 use App\Http\Controllers\Api\SearchController;
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('patients', PatientsController::class);
 });
 
-Auth::routes();
+Auth::routes();  // Esto ya incluye la ruta /login, no es necesario agregarla manualmente.
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-
+// Las siguientes rutas ya están incluidas en Auth::routes(), no es necesario agregar estas.
 Route::post('/login', [LoginController::class, 'login']);
-
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+// Ruta personalizada para eliminar y editar pacientes
 Route::get('delete/{id}', [PatientsController::class, 'destroy']);
 Route::get('edit/{id}', [PatientsController::class, 'edit']);
-Route::put('/patients/{id}', [PatientController::class, 'update'])->name('patients.update');
 
+// Rutas para generar PDF y buscar
 Route::get('generate-pdf/{id}', [PDFController::class, 'generatePDF'])->name('generate.pdf');
-
 Route::get('/', [SearchController::class, 'search'])->name('patients.search');
 Route::get('api/search', [SearchController::class, 'search']);
